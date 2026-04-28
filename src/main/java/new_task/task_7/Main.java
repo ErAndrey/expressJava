b0f4fadeac338e8e963e2439ef30958321283fbb
erandrew <eroshkin.andrey@bk.ru>
2026-04-29 03:09:42 +0400

    add: new_task

--- src/main/java/new_task/task_7/Main.java
+++ src/main/java/new_task/task_7/Main.java
@@ -9,5 +9,34 @@ public class Main {
         System.out.println("Баланс казино " + casino.getBalance() + "₽");
 
         casino.play();
+
+        /**
+         * toDo
+         * класс User , Player -> Зареганный в казино по user.name + password
+         *
+         * в login():
+         * "Добро пожаловать..."
+         * 0. Выход -> return
+         * 1. Логин : User -> Player -> menu()
+         * 2. Регистрация :
+         *      есть Player по User -> Ошибка, вернуть в login()
+         *      нет Player по User -> Регнуть -> menu()
+         *
+         * в menu():
+         * "Привет, player.name..."
+         * 0. Выйти -> login()
+         * 1. Баланс -> balance()
+         * 2. Играть -> play()
+         *
+         * в balance():
+         * "Ваш баланс: player.balance"
+         * 0. Назад -> menu()
+         * 1. Пополнение : User -> Player (по player.code)
+         * 2. Вывод : Player -> User (по player.code)
+         *
+         * в play():
+         * 0. Назад -> menu()
+         * 1-N. Игры
+         */
     }
 }
